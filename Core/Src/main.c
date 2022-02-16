97705f418c56e16b7e3c2713332a656baffb42b9
andremdaraujo <andremdaraujo@gmail.com>
2022-02-16 01:27:12 -0300

    Reviewing code for final report; minor adjustments and comments added.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -172,7 +172,7 @@ int main(void)
 	HAL_TIM_PWM_Start(&htim4, TIM_CHANNEL_1);	// Timer 4 for PWM generation
 
 	UART_TX_string("Smart Micro Pump\n");
-	UART_RX(rx_buffer);
+	UART_RX(rx_buffer);							// Starts serial reception
 
 	PID_init(&PID);
 
@@ -279,6 +279,8 @@ int main(void)
 				UART_TX_float(PID.output);
 				UART_TX_string("mA:");
 				UART_TX_float(pump_current);
+
+				// include temperature and VREF
 
 				UART_TX_string("\r ");
 			}
